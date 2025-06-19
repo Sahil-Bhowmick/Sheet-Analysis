8c91d2b5d09b9163ce4d1d51ce0514670b2d8917
Sahil Bhowmick <sahilbhowmick1@gmail.com>
2025-06-19 21:11:59 +0530

    Remove node_modules from Git tracking

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-center py-auto text-purple-600">
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
